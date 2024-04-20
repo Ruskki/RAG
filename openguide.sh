26e7534e2058c80e9d66a354a191cf889483e2dd
Chris <83106149+Ruskki@users.noreply.github.com>
2024-04-20 17:31:39 -0400

    Update openguide.sh

--- openguide.sh
+++ openguide.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tmux new-session \; split-window -h "curl https://raw.githubusercontent.com/Ruskki/RAG/main/README.md | less"
+tmux new-session \; split-window -h "curl -s https://raw.githubusercontent.com/Ruskki/RAG/main/README.md | less" \; resize-pane -R 30
